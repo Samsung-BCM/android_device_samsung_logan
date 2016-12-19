e8a7e7b986215588f2ba99299a757604dcd3d87f
ZIM <zim555@mail.ru>
2016-12-19 14:47:57 +0200

    Add gps and wifi configs

--- device.mk
+++ device.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
 # Get non-open-source specific aspects
 #$(call inherit-product-if-exists, vendor/samsung/logan/logan-vendor.mk)
 
@@ -8,3 +11,5 @@ DEVICE_PACKAGE_OVERLAYS +=  device/samsung/logan/overlay
 
 # common loganxx
 $(call inherit-product, device/samsung/bcm-common/loganxx.mk)
+
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
