e7779f293a078391212b20de1c047d486db2ccff
ZIM <zim555@mail.ru>
2016-12-23 21:25:08 +0200

    Add logan init

--- device.mk
+++ device.mk
@@ -9,6 +9,10 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS +=  device/samsung/logan/overlay
 
+PRODUCT_COPY_FILES += \
+    device/samsung/bcm-common/ramdisk/init.hawaii_ss_loganxx.rc:root/init.hawaii_ss_logan.rc \
+    device/samsung/bcm-common/ramdisk/ueventd.hawaii_ss_loganxx.rc:root/ueventd.hawaii_ss_logan.rc
+
 # common loganxx
 $(call inherit-product, device/samsung/bcm-common/loganxx.mk)
 
